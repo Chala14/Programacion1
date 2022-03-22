2060f3b219fee4894c53d44ecd835c72f739087d
superalexzapata <alexzapata0115@gmail.com>
2022-03-21 20:50:58 -0500

    Programa inicio 2

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Clear();
+            Console.WriteLine("Hola Alex");
+            Console.ReadLine();
+        }
+    }
+}
